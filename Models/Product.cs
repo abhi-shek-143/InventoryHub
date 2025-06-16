9eb23cf1bed040e7a29635ba734c790bc4d2c325
Abinayam2503 <abinayam771@gmail.com>
2025-06-16 18:01:07 +0530

    Added domain models Product and Supplier

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryHub.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();       // Unique identifier
+        public string Name { get; set; } = string.Empty;     // Product name
+        public string Description { get; set; }
+        public decimal Price { get; set; }                   // Price per unit
+        public int Quantity { get; set; }                    // Current stock level
+        public Guid SupplierId { get; set; }                 // Link to Supplier
+    }
+}
---
+++ Models/Supplier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryHub.Models
+{
+    public class Supplier
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();       // Unique identifier
+        public string Name { get; set; } = string.Empty;
+        public string ContactInfo { get; set; }
+    }
+}
